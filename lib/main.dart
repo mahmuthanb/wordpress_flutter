60bbbe7590df2bfc27ef5b8cde815224e5532caf
Mahmuthan BARIŞIK <mahmuthanb@gmail.com>
2023-01-15 21:46:53 +0300

    wrap main with blocprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
+import 'package:wordpress_flutter/app/presentation/connectivity/cubit/internet_cubit.dart';
 import 'package:wordpress_flutter/app/presentation/home_page/home_page.dart';
 import 'package:wordpress_flutter/core/di/locator.dart';
+import 'package:wordpress_flutter/core/widget/info_message.dart';
+import 'package:wordpress_flutter/core/widget/loading_view.dart';
 
 void main() {
   setupDI(Environment.dev);
@@ -13,13 +17,43 @@ class WordPressFlutter extends StatelessWidget {
   const WordPressFlutter({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    final Connectivity connectivity = Connectivity();
+    return MaterialApp(
       title: 'WordPress in Flutter',
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider<InternetCubit>(
+            create: (BuildContext context) =>
+                InternetCubit(connectivity: connectivity),
+          ),
+        ],
+        child: Scaffold(
+          body: BlocListener<InternetCubit, InternetState>(
+            listener: (context, state) {
+              InfoMessage(
+                      message: "Internet connected",
+                      status: MessageStatus.success)
+                  .snack(context);
+            },
+            child: BlocBuilder<InternetCubit, InternetState>(
+              builder: (context, state) {
+                if (state is InternetConnected &&
+                    state.connectivityResult != ConnectivityResult.none) {
+                  return HomePage();
+                } else if (state is InternetDisconnected) {
+                  return const LoadingScreen(title: "Disconnected");
+                } else {
+                  return const LoadingScreen();
+                }
+              },
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
