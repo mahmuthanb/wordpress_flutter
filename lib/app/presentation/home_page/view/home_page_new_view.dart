3a38a586fb2fe120e41ddf93a0904a84aff9ac44
Mahmuthan BARIŞIK <mahmuthanb@gmail.com>
2023-01-16 22:52:02 +0300

    ui fixes and dimens class

--- lib/app/presentation/home_page/view/home_page_new_view.dart
+++ lib/app/presentation/home_page/view/home_page_new_view.dart
@@ -4,6 +4,7 @@ import 'package:html_unescape/html_unescape.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:wordpress_flutter/app/data/model/post/post_model.dart';
 import 'package:wordpress_flutter/core/res/colors.dart';
+import 'package:wordpress_flutter/core/res/dimensions.dart';
 import 'package:wordpress_flutter/core/res/text_style.dart';
 
 class HomePageNewView extends StatelessWidget {
@@ -20,16 +21,17 @@ class HomePageNewView extends StatelessWidget {
             "MAHMUTHANCOM",
             style: AppTextStyle.headerTextStyle,
           ),
+          elevation: 0,
           centerTitle: false,
           actions: [
             IconButton(
               onPressed: () => Scaffold.of(context).openDrawer(),
-              icon: const Icon(
-                Icons.menu,
-              ),
+              icon: const Icon(Icons.menu),
+              color: AppColors.darkerThenGrey,
             )
           ],
         ),
+        backgroundColor: AppColors.primarySwatch,
         body: Builder(
           builder: (context) {
             if (postList.isEmpty) {
@@ -47,61 +49,94 @@ class HomePageNewView extends StatelessWidget {
                   baseColor: AppColors.shimmerBase,
                   highlightColor: AppColors.shimmerHighlighted);
             } else {
-              return ListView.builder(
-                itemCount: postList.length,
-                itemBuilder: (context, index) {
-                  PostModel data = postList[index];
-                  return Card(
-                      elevation: 1,
-                      child: GestureDetector(
-                        onTap: () => Navigator.of(context)
-                            .pushNamed('post-detail', arguments: data),
-                        child: Container(
-                          margin: const EdgeInsets.symmetric(
-                              vertical: 5, horizontal: 10),
-                          width: size.width,
-                          height: realHeight * .15,
-                          child: Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  SizedBox(
-                                    width: size.width * .97 - 20,
-                                    height: realHeight * .025,
-                                    child: Text(
-                                      HtmlUnescape().convert(
-                                          data.title!.rendered.toString()),
-                                      overflow: TextOverflow.ellipsis,
-                                      style: const TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                      maxLines: 1,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  (data.excerpt != null &&
-                                          data.excerpt!.rendered != null)
-                                      ? SizedBox(
-                                          width: size.width * .9,
-                                          height: realHeight * .125,
-                                          child: Center(
-                                            child: Html(
-                                              data: data.excerpt!.rendered,
-                                            ),
-                                          ),
-                                        )
-                                      : Container(),
-                                ],
-                              ),
-                            ],
+              return Column(
+                children: [
+                  SizedBox(
+                    height: size.height * .05,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        border: Border.symmetric(
+                          horizontal: BorderSide(
+                            color: AppColors.darkGrey,
+                            width: 2,
                           ),
                         ),
-                      ));
-                },
+                      ),
+                      child: ListView.separated(
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            return Text('CATEGORY $index');
+                          },
+                          separatorBuilder: (context, index) =>
+                              const SizedBox(width: AppDimens.xxs),
+                          itemCount: 6),
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: postList.length,
+                      itemBuilder: (context, index) {
+                        PostModel data = postList[index];
+                        return Card(
+                            elevation: 0,
+                            color: Colors.transparent,
+                            child: GestureDetector(
+                              onTap: () => Navigator.of(context)
+                                  .pushNamed('post-detail', arguments: data),
+                              child: Container(
+                                margin: const EdgeInsets.symmetric(
+                                  vertical: AppDimens.s,
+                                  horizontal: AppDimens.s,
+                                ),
+                                width: size.width,
+                                height: realHeight * .15,
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        SizedBox(
+                                          width: size.width * .97 - 20,
+                                          height: realHeight * .025,
+                                          child: Text(
+                                            HtmlUnescape().convert(data
+                                                .title!.rendered
+                                                .toString()),
+                                            overflow: TextOverflow.ellipsis,
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                            maxLines: 1,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        (data.excerpt != null &&
+                                                data.excerpt!.rendered != null)
+                                            ? SizedBox(
+                                                width: size.width * .9,
+                                                height: realHeight * .125,
+                                                child: Center(
+                                                  child: Html(
+                                                    data:
+                                                        data.excerpt!.rendered,
+                                                  ),
+                                                ),
+                                              )
+                                            : Container(),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ));
+                      },
+                    ),
+                  ),
+                ],
               );
             }
           },
---
+++ lib/core/res/dimensions.dart
@@ -0,0 +1,22 @@
+class AppDimens {
+  /// 2.0
+  static const double xxs = 2.0;
+
+  /// 4.0
+  static const double xs = 4.0;
+
+  /// 8.0
+  static const double s = 8.0;
+
+  /// 16.0
+  static const double m = 16.0;
+
+  /// 24.0
+  static const double l = 24.0;
+
+  /// 32.0
+  static const double xl = 32.0;
+
+  /// 48.0
+  static const double xxl = 48.0;
+}
