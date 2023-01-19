ed8b77d0fb9e045b1fcb9c671cd27a58e7a5be74
Mahmuthan <mahmuthanb@gmail.com>
2023-01-19 22:24:31 +0300

    wellcome page fix

--- lib/app/presentation/wellcome/cubit/wellcome_cubit.dart
+++ lib/app/presentation/wellcome/cubit/wellcome_cubit.dart
@@ -1,8 +1,11 @@
 import 'package:bloc/bloc.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:meta/meta.dart';
 
 part 'wellcome_state.dart';
 
 class WellcomeCubit extends Cubit<WellcomeState> {
-  WellcomeCubit() : super(WellcomeInitial());
+  WellcomeCubit(this.getStorage) : super(WellcomeInitial());
+  GetStorage getStorage;
+  Future onSave(String? value) => getStorage.write("domain", value);
 }
--- lib/app/presentation/wellcome/view/wellcome_page.dart
+++ lib/app/presentation/wellcome/view/wellcome_page.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wordpress_flutter/app/presentation/home_page/home_page.dart';
+import 'package:wordpress_flutter/app/presentation/wellcome/cubit/wellcome_cubit.dart';
 import 'package:wordpress_flutter/core/res/dimensions.dart';
+import 'package:wordpress_flutter/core/util/validator.dart';
 
-class WellcomePage extends StatelessWidget {
+class WellcomePage extends StatefulWidget {
   const WellcomePage({super.key});
 
+  @override
+  State<WellcomePage> createState() => _WellcomePageState();
+}
+
+class _WellcomePageState extends State<WellcomePage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _domainController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,17 +28,17 @@ class WellcomePage extends StatelessWidget {
             children: [
               RichText(
                 text: TextSpan(
-                  text: 'Latest article with ',
+                  text: 'Latest article \nwith ',
                   style:
                       DefaultTextStyle.of(context).style.copyWith(fontSize: 44),
                   children: [
                     TextSpan(
-                      text: 'better',
+                      text: ' better ',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.normal,
                         backgroundColor: Colors.yellow.shade200,
                         decorationStyle: TextDecorationStyle.solid,
-                        letterSpacing: 1,
                       ),
                     ),
                     const TextSpan(text: ' news!'),
@@ -41,6 +52,32 @@ class WellcomePage extends StatelessWidget {
                 child: const Text(
                     "Browse the best news on latest strategies, trends various tools and news in the world"),
               ),
+              Form(
+                key: _formKey,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.red,
+                          style: BorderStyle.solid,
+                        ),
+                      ),
+                      labelText: "Domain",
+                      hintText: 'http:// or https://',
+                      // prefixText: "http",
+                      labelStyle: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  controller: _domainController,
+                  validator: Validator.url,
+                  onFieldSubmitted: (newValue) =>
+                      _formKey.currentState!.validate(),
+                ),
+              ),
+              const SizedBox(
+                height: AppDimens.l,
+              ),
               Container(
                 decoration: BoxDecoration(
                   color: Colors.yellow.shade200,
@@ -52,59 +89,62 @@ class WellcomePage extends StatelessWidget {
                     ),
                   ],
                 ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 30,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: AppDimens.m,
-                      ),
-                      child: const Center(
-                        child: Text("Get started"),
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 1.5,
+                child: GestureDetector(
+                  onTap: () {
+                    if (_formKey.currentState!.validate()) {
+                      context
+                          .read<WellcomeCubit>()
+                          .onSave(_domainController.text)
+                          .then(
+                            (value) => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => HomePage(),
+                              ),
+                            ),
+                          );
+                    }
+                  },
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 40,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: AppDimens.m,
+                        ),
+                        child: const Center(
+                          child: Text("Get started"),
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 1.5,
+                          ),
                         ),
                       ),
-                    ),
-                    // Container(
-                    //   color: Colors.black,
-                    //   width: 15,
-                    // ),
-                    Container(
-                      height: 30,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: AppDimens.xs,
-                      ),
-                      decoration: const BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(color: Colors.black, width: 1.5),
-                          top: BorderSide(color: Colors.black, width: 1.5),
-                          right: BorderSide(color: Colors.black, width: 1.5),
+                      Container(
+                        height: 40,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: AppDimens.xs,
                         ),
-                      ),
-                      child: const Icon(
-                        Icons.arrow_right_alt,
-                      ),
-                    )
-                  ],
+                        decoration: const BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(color: Colors.black, width: 1.5),
+                            top: BorderSide(color: Colors.black, width: 1.5),
+                            right: BorderSide(color: Colors.black, width: 1.5),
+                          ),
+                        ),
+                        child: const Icon(
+                          Icons.arrow_right_alt,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-              // ElevatedButton(
-              //   onPressed: () {},
-              //   child:
-              //   style: ButtonStyle(
-              //     backgroundColor:
-              //         MaterialStateProperty.all(Colors.yellow.shade200),
-              //     shadowColor: MaterialStateProperty.all(Colors.amber),
-
-              //   ),
-              // )
             ],
           ),
         ),
