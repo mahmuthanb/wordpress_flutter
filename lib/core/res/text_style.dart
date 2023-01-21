7f6623d6e054ad14f5325ef920ce61f2fa575735
Mahmuthan BARIŞIK <mahmuthanb@gmail.com>
2023-01-21 23:31:30 +0300

    Delete text_style.dart

--- lib/core/res/text_style.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:wordpress_flutter/core/res/colors.dart';
-
-class AppTextStyle {
-  static TextStyle headerTextStyle = GoogleFonts.openSans(
-      fontWeight: FontWeight.bold, color: AppColors.darkerThenGrey);
-}
