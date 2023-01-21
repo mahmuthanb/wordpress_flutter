f2a53193affb1706769bf6aac6f889623daf22cc
Mahmuthan BARIŞIK <mahmuthanb@gmail.com>
2023-01-21 18:13:19 +0300

    timegao package

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:wordpress_flutter/app/presentation/home_page/home_page.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(HomePage());
+    await tester.pumpWidget(const HomePage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
