e0e2a35700f6d1dbfa9c84c754e1478ce3ee80d5
Mahmuthan BARIŞIK <mahmuthanb@gmail.com>
2023-01-29 20:35:33 +0300

    Router moved to new folder

--- lib/app/router/router.dart
+++ lib/app/router/router.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:route_map/route_map.dart';
-import 'package:wordpress_flutter/app/router.routes.dart';
+import 'package:wordpress_flutter/app/router/router.routes.dart';
 
 @RouteMapInit()
 Route? onGenerateRoute(RouteSettings routeSettings) =>
