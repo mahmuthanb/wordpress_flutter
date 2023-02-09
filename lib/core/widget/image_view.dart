75c65f4828576ac3b55ab9bb99af7203449b58a4
Mahmuthan <mahmuthanb@gmail.com>
2023-02-09 21:22:57 +0300

    imageview zoomi in/out feature

--- lib/core/widget/image_view.dart
+++ lib/core/widget/image_view.dart
@@ -14,18 +14,20 @@ class ImageView extends StatelessWidget {
   final String url;
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
+    final PhotoViewController photoViewController = PhotoViewController();
+    // final Size size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: AppColors.green,
+      backgroundColor: AppColors.white,
       appBar: AppBar(
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        title: Text(url),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => photoViewController.setScaleInvisibly(1.5),
             icon: const Icon(Icons.zoom_in),
           ),
           IconButton(
-            onPressed: () {},
+            onPressed: () => photoViewController.setScaleInvisibly(1),
             icon: const Icon(Icons.zoom_out),
           )
         ],
@@ -41,7 +43,7 @@ class ImageView extends StatelessWidget {
                   color: AppColors.white,
                   backgroundBlendMode: BlendMode.srcOver,
                 ),
-                controller: PhotoViewController(),
+                controller: photoViewController,
                 imageProvider: CachedNetworkImageProvider(url),
               ),
             ),
